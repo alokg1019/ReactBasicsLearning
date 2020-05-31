90f7bfcfc1574f7a4ccfe8aa9703789162170494
Alok Goyal <alokg1019@gmail.com>
2020-05-30 18:14:55 -0700

    passing method references

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import {UseState} from 'react';
  * Using Class Component
  * 
  **/ 
-/*
+
 class App extends Component {
 
   state = {
@@ -29,12 +29,10 @@ class App extends Component {
     ]
   }
 
-  switchNameHandler = () => {
-    console.log("Was Clicked!");
-    this.state.persons[0].name = "Alok";
+  switchNameHandler = (newName) => {
 
     this.setState({ persons: [
-      { name: 'Alok', age: 28},
+      { name: newName, age: 28},
       { name: 'Manu', age: 29},
       { name: 'Stephanie', age: 30},
     ]});
@@ -45,46 +43,55 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi, I'm a rect App</h1>
-        <button onClick={ this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobiies: Racing</Person>
+        <button onClick={ () => this.switchNameHandler('Alok') }>Switch Name</button>
+        <Person
+         name={this.state.persons[0].name}
+          age={this.state.persons[0].age}> 
+        </Person>
+        <Person
+         name={this.state.persons[1].name}
+         age={this.state.persons[1].age}
+         click={this.switchNameHandler.bind(this,"Alok!!")}
+         >My Hobies: Racing
+        </Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
       </div>
     )
   }
+
 }
-*/
+
 
 // Using ReactHooks : New Feature since React 16.8 - Allows to update state in function based components
-const App = props => {
+// const App = props => {
 
-  const [personsStates, setPersonsState ] = useState({
-    persons : [
-      { name: 'Max', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 30},
-    ]
-  });
+//   const [personsStates, setPersonsState ] = useState({
+//     persons : [
+//       { name: 'Max', age: 28},
+//       { name: 'Manu', age: 29},
+//       { name: 'Stephanie', age: 30},
+//     ]
+//   });
 
-  const switchNameHandler = () => {
-    console.log("Was Clicked!");  
-    setPersonsState({ persons: [
-      { name: 'Alok', age: 28},
-      { name: 'Manu', age: 29},
-      { name: 'Stephanie', age: 30},
-    ]});
-  }
+//   const switchNameHandler = () => {
+//     console.log("Was Clicked!");  
+//     setPersonsState({ persons: [
+//       { name: 'Alok', age: 28},
+//       { name: 'Manu', age: 29},
+//       { name: 'Stephanie', age: 30},
+//     ]});
+//   }
 
-  return (
-    <div className="App">
-      <h1>Hi, I'm a rect App</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsStates.persons[0].name} age={personsStates.persons[0].age}></Person>
-      <Person name={personsStates.persons[1].name} age={personsStates.persons[1].age}>My Hobiies: Racing</Person>
-      <Person name={personsStates.persons[2].name} age={personsStates.persons[2].age}></Person>
-    </div>
-  )
+//   return (
+//     <div className="App">
+//       <h1>Hi, I'm a rect App</h1>
+//       <button onClick={switchNameHandler}>Switch Name</button>
+//       <Person name={personsStates.persons[0].name} age={personsStates.persons[0].age}></Person>
+//       <Person name={personsStates.persons[1].name} age={personsStates.persons[1].age}>My Hobiies: Racing</Person>
+//       <Person name={personsStates.persons[2].name} age={personsStates.persons[2].age}></Person>
+//     </div>
+//   )
 
-}
+// }
 
 export default App;
