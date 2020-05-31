7d35c6819012220ad061170f16139625b1faf556
Alok Goyal <alokg1019@gmail.com>
2020-05-30 18:33:55 -0700

    styling component

--- src/App.js
+++ src/App.js
@@ -49,10 +49,19 @@ class App extends Component {
 
   render(){
 
+    // Inline Styling
+    const style = {
+      backgroundColor : 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor : 'pointer'
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a rect App</h1>
-        <button onClick={ () => this.switchNameHandler('Alok') }>Switch Name</button>
+        <button style={ style} onClick={ () => this.switchNameHandler('Alok') }>Switch Name</button>
         <Person
          name={this.state.persons[0].name}
           age={this.state.persons[0].age}> 
