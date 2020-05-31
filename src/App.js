a4938f8e04a8d19044894d1e2964003102666d8f
Alok Goyal <alokg1019@gmail.com>
2020-05-30 17:51:06 -0700

    playing with props and state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,90 @@
-import React from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person';
 
-function App() {
+//Using react Hook
+import {UseState} from 'react';
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hi, I'm a react app</h1>
+//     </div>
+//   );
+// }
+
+/** 
+ * 
+ * Using Class Component
+ * 
+ **/ 
+/*
+class App extends Component {
+
+  state = {
+    persons : [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 30},
+    ]
+  }
+
+  switchNameHandler = () => {
+    console.log("Was Clicked!");
+    this.state.persons[0].name = "Alok";
+
+    this.setState({ persons: [
+      { name: 'Alok', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 30},
+    ]});
+  }
+
+  render(){
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a rect App</h1>
+        <button onClick={ this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}></Person>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobiies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}></Person>
+      </div>
+    )
+  }
+}
+*/
+
+// Using ReactHooks : New Feature since React 16.8 - Allows to update state in function based components
+const App = props => {
+
+  const [personsStates, setPersonsState ] = useState({
+    persons : [
+      { name: 'Max', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 30},
+    ]
+  });
+
+  const switchNameHandler = () => {
+    console.log("Was Clicked!");  
+    setPersonsState({ persons: [
+      { name: 'Alok', age: 28},
+      { name: 'Manu', age: 29},
+      { name: 'Stephanie', age: 30},
+    ]});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hi, I'm a rect App</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsStates.persons[0].name} age={personsStates.persons[0].age}></Person>
+      <Person name={personsStates.persons[1].name} age={personsStates.persons[1].age}>My Hobiies: Racing</Person>
+      <Person name={personsStates.persons[2].name} age={personsStates.persons[2].age}></Person>
     </div>
-  );
+  )
+
 }
 
 export default App;
