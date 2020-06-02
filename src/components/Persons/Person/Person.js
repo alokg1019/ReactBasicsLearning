2065d4e011d55d09540afa77cbf3d9caba7b2656
Alok Goyal <alokg1019@gmail.com>
2020-06-01 18:06:05 -0700

    aux component

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,5 +1,6 @@
 import React, { Component} from 'react';
 import Radium from 'radium';
+import Aux from './../../../hoc/Aux';
 import './Person.css';
 
 const style = {
@@ -12,12 +13,16 @@ class Person extends Component {
 // const person = (props) => {
 render(){
 
+
+        // Or can also use Fragment - in built react component
         return (
-            <div className="Person" style={this.style}>
-                <p onClick={this.props.click}>I'm a { this.props.name} and I am { this.props.age} years old</p>
-                <p>{ this.props.children }</p>
-                <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            <Aux>
+                <div className="Person" style={this.style}>
+                    <p onClick={this.props.click}>I'm a { this.props.name} and I am { this.props.age} years old</p>
+                    <p>{ this.props.children }</p>
+                    <input type="text" onChange={this.props.changed} value={this.props.name} />
+                </div>
+            </Aux>
         )
     }
 }
---
+++ src/hoc/Aux.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+//Empty wrapper
+const aux = props => props.children;
+
+export default aux;
